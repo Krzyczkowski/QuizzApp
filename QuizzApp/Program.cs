01692447dfceb5ad9ad437ef6276e66d8c4eb123
krzyczkowski <krzyczkowski.wiktor00@gmail.com>
2024-11-20 18:53:12 +0100

    18:52.20.11.2024

--- QuizzApp/Program.cs
+++ QuizzApp/Program.cs
@@ -4,6 +4,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();  
 
 var app = builder.Build();
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();  
 app.MapControllers(); 
 app.Run();  
