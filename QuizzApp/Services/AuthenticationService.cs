614f01116ed099834fcd5237aab4bd7b3e07937d
krzyczkowski <krzyczkowski.wiktor00@gmail.com>
2024-12-03 00:37:03 +0100

    added Authentication Service

--- QuizzApp/Services/AuthenticationService.cs
+++ QuizzApp/Services/AuthenticationService.cs
@@ -3,24 +3,58 @@ using QuizzApp.Contracts.Authentication;
 using QuizzApp.Interfaces.Services;
 using QuizzApp.Interfaces.Persistance;
 using QuizzApp.Services;
+using QuizzApp.Models;
 public class AuthenticationService : IAuthenticationService
 {
+
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
     private readonly IUserRepository _userRepository;
 
-
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository){
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
         _jwtTokenGenerator = jwtTokenGenerator;
         _userRepository = userRepository;
     }
 
     public AuthenticationResult Login(LoginRequest loginRequest)
     {
-        throw new NotImplementedException();
-    }
+        var user = _userRepository.GetUserByEmail(loginRequest.Email);
+        if(user is null){
+            throw new Exception("User with given email doesnt exists.");
+        }
+        if(user.Password != loginRequest.Password){
+            throw new Exception("Invalid password.");
+        }
+        
+        // generate JWT token
+        var token = _jwtTokenGenerator.GenerateToken(user);
 
+        return new AuthenticationResult(
+            user,
+            token
+        );
+    }
     public AuthenticationResult Register(RegisterRequest registerRequest)
     {
-        throw new NotImplementedException();
+
+        //chceck if user exists
+        if (_userRepository.GetUserByEmail(registerRequest.Email) is not null)
+        {
+            throw new Exception("User with given email already exists.");
+        }
+        var user = new User(registerRequest.FirstName,
+          registerRequest.LastName,
+          registerRequest.Email,
+          registerRequest.Password
+          );
+          //add user
+        _userRepository.Add(user);
+        // generate JWT token
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(
+            user,
+            token
+        );
     }
 }
